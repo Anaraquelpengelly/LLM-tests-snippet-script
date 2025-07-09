52a98689839f342fc1dde55788ee2d6da3f85c0e
Ana Pengelly <anapengelly@Anas-MacBook-Pro.local>
2025-07-09 17:17:04 +0100

    added all files

---
+++ original_scripts/original_67.java
@@ -0,0 +1,1 @@
+package com.scalesec.vulnado;import org.springframework.web.bind.annotation.*;import org.springframework.boot.autoconfigure.*;import java.io.Serializable;@RestController@EnableAutoConfigurationpublic class CowController {    @RequestMapping(value = "/cowsay")    String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {        return Cowsay.run(input);    }}
